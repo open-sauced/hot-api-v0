fe4e428ee414b51be9d5f94bade57dc1dca4f14b
Brian Douglas <bdougie@users.noreply.github.com>
2021-12-26 08:29:06 -0500

    remove extra context (#8)

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,13 +1,2 @@
-export const NEXT_PUBLIC_SUPABASE_URL = process.env.SUPABASE_URL
-export const NEXT_PUBLIC_SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY
-
-export const DEFAULT_AVATARS_BUCKET = 'avatars'
-
-export type Profile = {
-  id: string
-  avatar_url: string
-  username: string
-  website: string
-  updated_at: string
-}
-
+export const NEXT_PUBLIC_SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL
+export const NEXT_PUBLIC_SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
